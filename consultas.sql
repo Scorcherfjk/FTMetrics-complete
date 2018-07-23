a21abed312d97c777ef06a037882ed637b797469
Fjavier De Freitas <32197704+Scorcherfjk@users.noreply.github.com>
2018-07-23 11:48:23 -0500

    acomodando

--- consultas.sql
+++ consultas.sql
@@ -75,17 +75,17 @@ ORDER BY cwc.sShortName
 
 
 SELECT wc.lOEEWorkCellId,
-								cwc.sShortName,
-								wc.sPartId,
-								wc.tStart,
-								wc.tEnd,
-								wc.dPartCount,
-								wc.dTotalParts,
-								wc.dScrapParts
-						FROM FTMetrics.dbo.OEEWorkCell as wc
-							,FTMetrics.dbo.OEEConfigWorkCell as cwc
-						WHERE wc.lOEEConfigWorkCellId = cwc.lOEEConfigWorkCellId
-								AND wc.lOEEConfigWorkCellId = $opcion
-								AND (wc.tStart >= '$inicio' OR wc.tEnd <= '$final')
-								AND NOT (wc.tStart < '$inicio' OR wc.tEnd > '$final')
-						ORDER BY cwc.sShortName
+		cwc.sShortName,
+		wc.sPartId,
+		wc.tStart,
+		wc.tEnd,
+		wc.dPartCount,
+		wc.dTotalParts,
+		wc.dScrapParts
+FROM FTMetrics.dbo.OEEWorkCell as wc
+	,FTMetrics.dbo.OEEConfigWorkCell as cwc
+WHERE wc.lOEEConfigWorkCellId = cwc.lOEEConfigWorkCellId
+		AND wc.lOEEConfigWorkCellId = $opcion
+		AND (wc.tStart >= '$inicio' OR wc.tEnd <= '$final')
+		AND NOT (wc.tStart < '$inicio' OR wc.tEnd > '$final')
+ORDER BY cwc.sShortName
