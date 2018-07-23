193ddf83e87b15d829dca2f83480590f514c5ac2
Fjavier De Freitas <32197704+Scorcherfjk@users.noreply.github.com>
2018-07-23 11:48:39 -0500

    probando

--- prueba.php
+++ prueba.php
@@ -3,43 +3,12 @@
 include('./conexion.php');
 include('./consultas.php');
 
-/*$query = "SELECT TOP 10 
-	[dTotalParts]
-	,[dGoodParts]
-	,[dScrapParts]
-  FROM [FTMetrics].[dbo].[OEEStateData]
-  WHERE [lOEEConfigWorkCellId]= 1000031";*/
 
-$query = "  SELECT						
-TOP 10						
-cwc.sShortName
-,wc.sPartId
-,wc.tStart
-,wc.tEnd
-,wc.dPartCount
-,wc.dTotalParts
-,wc.dScrapParts
-FROM FTMetrics.dbo.OEEWorkCell as wc
-,FTMetrics.dbo.OEEConfigWorkCell as cwc
-WHERE wc.lOEEConfigWorkCellId = cwc.lOEEConfigWorkCellId
-ORDER BY cwc.sShortName";
+$opcion = $_POST['seleccion'];
+$inicio = $_POST['inicio'];
+$final = $_POST['final'];
 
+$query = primaryQuery($opcion,$inicio,$final);
 $prep = sqlsrv_prepare($conn,$query);
-//echo "prep ".$prep;
-
-//echo "<br>";
-
 $resultado = sqlsrv_execute($prep);
-//echo "resultado ".$resultado;
-
-echo "<br>";
-
-$fila = sqlsrv_fetch_array($prep);
-echo json_encode($fila);
-
-echo "<br>";
-
-echo($fila['tStart']->date);
-
-echo $_POST['id'];
 
