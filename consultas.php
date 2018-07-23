18ea7cec6a4e9f656ed7632f7053342a1c358fc8
Fjavier De Freitas <32197704+Scorcherfjk@users.noreply.github.com>
2018-07-23 11:57:33 -0500

    reubicando

--- consultas.php
+++ consultas.php
@@ -1,5 +1,23 @@
 <?php
 
+function defaultQuery()
+{
+    $query = "SELECT 							
+            TOP 1000						
+            cwc.sShortName
+            ,wc.sPartId
+            ,SUM(wc.dPartCount) AS dPartCount
+            ,SUM(wc.dTotalParts) AS dTotalParts
+            ,SUM(wc.dScrapParts) AS dScrapParts
+        FROM FTMetrics.dbo.OEEWorkCell as wc
+            ,FTMetrics.dbo.OEEConfigWorkCell as cwc
+        WHERE wc.lOEEConfigWorkCellId = cwc.lOEEConfigWorkCellId
+        GROUP BY cwc.sShortName, wc.sPartId
+        ORDER BY cwc.sShortName";
+
+    return $query;
+}
+
 function primaryQuery($opcion,$inicio,$final)
 {
     $query = "SELECT wc.lOEEWorkCellId,
@@ -17,24 +35,6 @@ function primaryQuery($opcion,$inicio,$final)
             AND (wc.tStart >= '$inicio' OR wc.tEnd <= '$final')
             AND NOT (wc.tStart < '$inicio' OR wc.tEnd > '$final')
         ORDER BY wc.tStart";
-
-    return $query;
-}
-
-function defaultQuery()
-{
-    $query = "SELECT 							
-            TOP 1000						
-            cwc.sShortName
-            ,wc.sPartId
-            ,SUM(wc.dPartCount) AS dPartCount
-            ,SUM(wc.dTotalParts) AS dTotalParts
-            ,SUM(wc.dScrapParts) AS dScrapParts
-        FROM FTMetrics.dbo.OEEWorkCell as wc
-            ,FTMetrics.dbo.OEEConfigWorkCell as cwc
-        WHERE wc.lOEEConfigWorkCellId = cwc.lOEEConfigWorkCellId
-        GROUP BY cwc.sShortName, wc.sPartId
-        ORDER BY cwc.sShortName";
 
     return $query;
 }
