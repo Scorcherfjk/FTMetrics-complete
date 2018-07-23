a07b66666ba5d5e9fba39d5f4f7aab6aae7c9d24
Fjavier De Freitas <32197704+Scorcherfjk@users.noreply.github.com>
2018-07-23 17:31:50 -0500

    añadiendo funcion de validacion

--- funciones.php
+++ funciones.php
@@ -24,4 +24,26 @@ function total($array, $columName) {
     return array_sum(array_column($array, $columName));
 }
 
+function validar($array1, $array2){
+
+    if ($array1 != []){ 
+        $dTotalParts = total($array1, 'dTotalParts');
+        $dPartCount = total($array1, 'dScrapParts');
+        $dScrapParts = total($array1, 'dPartCount');
+        $array = array($dTotalParts, $dPartCount, $dScrapParts);
+        foreach ($array as $arr) {
+            echo("<br/>");
+            echo $arr;
+        }
+    } elseif ($array2 != []){
+        $dTotalParts = total($array2, 'dTotalParts');
+        $dPartCount = total($array2, 'dScrapParts');
+        $dScrapParts = total($array2, 'dPartCount');
+        $array = array($dTotalParts, $dPartCount, $dScrapParts);
+        foreach ($array as $arr) {
+            echo("<br/>");
+            echo $arr;
+        }
+    }
+}
 ?>
