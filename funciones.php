2337bf2450b1c289ad43fc3bb7cd3cf6a0413f4a
Fjavier De Freitas <32197704+Scorcherfjk@users.noreply.github.com>
2018-07-24 09:52:47 -0500

    funciones mejoradas

--- funciones.php
+++ funciones.php
@@ -26,25 +26,29 @@ function total($array, $columName) {
 
 function validar($array1, $array2){
 
-    if ($array1 != []){ 
+    if ($array1 != []){
+        json_encode($array1);
         $Id = $array1[0]['lOEEConfigWorkCellId'] ;
+        $shortName = $array1[0]['sShortName'] ;
         $partId = $array1[0]['sPartId'] ;
-        $fechaI = $array1[0]['tStart']->date;
-        $fechaF = end($array1)['tEnd']->date;
+        $fechaI = substr($array1[0]['tStart']->date, 0, 19);
+        $fechaF = substr(end($array1)['tEnd']->date, 0, 19);
         $dTotalParts = total($array1, 'dTotalParts');
         $dPartCount = total($array1, 'dScrapParts');
         $dScrapParts = total($array1, 'dPartCount');
-        $array = array($Id, $partId, $fechaI, $fechaF, $dTotalParts, $dPartCount, $dScrapParts);
+        $array = array( 'lOEEConfigWorkCellId' => $Id, 'sShortName' => $shortName,'sPartId' =>  $partId,'tStart' => $fechaI,'tEnd' => $fechaF,'dTotalParts' => $dTotalParts,'dScrapParts' =>  $dScrapParts,'dPartCount' =>  $dPartCount);
         return $array;
     } elseif ($array2 != []){
+        json_encode($array2);
         $Id = $array2[0]['lOEEConfigWorkCellId'] ;
+        $shortName = $array2[0]['sShortName'] ;
         $partId = $array2[0]['sPartId'] ;
-        $fechaI = $array2[0]['tStart']->date;
-        $fechaF = end($array2)['tEnd']->date;
+        $fechaI = substr($array2[0]['tStart']->date, 0, 19);
+        $fechaF = substr(end($array2)['tEnd']->date, 0, 19);
         $dTotalParts = total($array2, 'dTotalParts');
         $dPartCount = total($array2, 'dScrapParts');
         $dScrapParts = total($array2, 'dPartCount');
-        $array = array($Id, $partId, $fechaI, $fechaF, $dTotalParts, $dPartCount, $dScrapParts);
+        $array = array( 'lOEEConfigWorkCellId' => $Id, 'sShortName' => $shortName,'sPartId' =>  $partId,'tStart' => $fechaI,'tEnd' => $fechaF,'dTotalParts' => $dTotalParts,'dScrapParts' =>  $dScrapParts,'dPartCount' =>  $dPartCount);
         return $array;
     }
 }
