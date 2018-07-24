d9bb7d65a09c005c57eeb76fc9a772f73cf26df3
Fjavier De Freitas <32197704+Scorcherfjk@users.noreply.github.com>
2018-07-24 09:53:11 -0500

    pruebas listas

--- prueba3.php
+++ prueba3.php
@@ -20,18 +20,22 @@ if ( $resultado = sqlsrv_execute($prep) ) {
 
 					//pasando las fechas a variables
 					json_encode($fila);
-					$fechaInicio = substr($fila['tStart']->date, 0, 19);
-                    $fechaFinal = substr($fila['tEnd']->date, 0, 19);
 
                     if ($fila['sPartId'] == '16 oz'){
                         if ($datos12 != []){
-                            echo "fecha 12 oz: ".max($datos12);
-                            echo("<br/>");                            
-                            echo "dTotalParts 12 oz: ".total($datos12, 'dTotalParts');
+                            echo $datos12[0]['sShortName'];
                             echo("<br/>");
-                            echo "dPartCount 12 oz: ".total($datos12, 'dPartCount');
+                            echo $datos12[0]['sPartId'];
                             echo("<br/>");
-                            echo "dScrapParts 12 oz: ".total($datos12, 'dScrapParts');
+                            echo $datos12[0]['tStart']->date;
+                            echo("<br/>");
+                            echo end($datos12)['tEnd']->date;
+                            echo("<br/>");                             
+                            echo total($datos12, 'dTotalParts');
+                            echo("<br/>");
+                            echo total($datos12, 'dPartCount');
+                            echo("<br/>");
+                            echo total($datos12, 'dScrapParts');
                             echo("<br/>");
                             echo("<br/>");
                             echo("<br/>");
@@ -40,11 +44,19 @@ if ( $resultado = sqlsrv_execute($prep) ) {
                             $datos12 = array();
                     }else{
                         if ($datos16 != []){
-                            echo "dTotalParts 16 oz: ".total($datos16, 'dTotalParts');
+                            echo $datos16[0]['lOEEConfigWorkCellId'];
                             echo("<br/>");
-                            echo "dPartCount 16 oz: ".total($datos16, 'dPartCount');
+                            echo $datos16[0]['sPartId'];
                             echo("<br/>");
-                            echo "dScrapParts 16 oz: ".total($datos16, 'dScrapParts');
+                            echo $datos16[0]['tStart']->date;
+                            echo("<br/>");
+                            echo end($datos16)['tEnd']->date;
+                            echo("<br/>"); 
+                            echo total($datos16, 'dTotalParts');
+                            echo("<br/>");
+                            echo total($datos16, 'dPartCount');
+                            echo("<br/>");
+                            echo total($datos16, 'dScrapParts');
                             echo("<br/>");
                             echo("<br/>");
                             echo("<br/>");
@@ -54,7 +66,8 @@ if ( $resultado = sqlsrv_execute($prep) ) {
                     }
                 }
                     
-                validar($datos12, $datos16);
+                $array = validar($datos12, $datos16);
+                print_r($array);
                     
                 }    
 ?>
