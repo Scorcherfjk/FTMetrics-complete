9a897b17919cd6873b3498ee4df9261c4c4471d1
Fjavier De Freitas <32197704+Scorcherfjk@users.noreply.github.com>
2018-07-24 08:37:12 -0500

    mejorando la devolucion de los datos

--- funciones.php
+++ funciones.php
@@ -27,23 +27,25 @@ function total($array, $columName) {
 function validar($array1, $array2){
 
     if ($array1 != []){ 
+        $Id = $array1[0]['lOEEConfigWorkCellId'] ;
+        $partId = $array1[0]['sPartId'] ;
+        $fechaI = $array1[0]['tStart']->date;
+        $fechaF = end($array1)['tEnd']->date;
         $dTotalParts = total($array1, 'dTotalParts');
         $dPartCount = total($array1, 'dScrapParts');
         $dScrapParts = total($array1, 'dPartCount');
-        $array = array($dTotalParts, $dPartCount, $dScrapParts);
-        foreach ($array as $arr) {
-            echo("<br/>");
-            echo $arr;
-        }
+        $array = array($Id, $partId, $fechaI, $fechaF, $dTotalParts, $dPartCount, $dScrapParts);
+        return $array;
     } elseif ($array2 != []){
+        $Id = $array2[0]['lOEEConfigWorkCellId'] ;
+        $partId = $array2[0]['sPartId'] ;
+        $fechaI = $array2[0]['tStart']->date;
+        $fechaF = end($array2)['tEnd']->date;
         $dTotalParts = total($array2, 'dTotalParts');
         $dPartCount = total($array2, 'dScrapParts');
         $dScrapParts = total($array2, 'dPartCount');
-        $array = array($dTotalParts, $dPartCount, $dScrapParts);
-        foreach ($array as $arr) {
-            echo("<br/>");
-            echo $arr;
-        }
+        $array = array($Id, $partId, $fechaI, $fechaF, $dTotalParts, $dPartCount, $dScrapParts);
+        return $array;
     }
 }
 ?>
